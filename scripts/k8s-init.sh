8f9fb830f0c4ec5afc754513da7318444308c5d0
Paul Baines <paul.baines@gmail.com>
2022-07-26 16:42:42 -0700

    Basic tweaks and scaffolding for istio and kiali

--- scripts/k8s-init.sh
+++ scripts/k8s-init.sh
@@ -3,6 +3,7 @@
 NAMESPACE=ultrasearch
 
 kubectl create namespace ${NAMESPACE}
+kubectl label namespace ${NAMESPACE} istio-injection=enabled
 kubectl create secret generic broker --from-literal=url="${BROKER_URL}" --namespace=${NAMESPACE}
 kubectl create secret generic supabase --from-literal=url="${SUPABASE_URL}" --from-literal=key="${SUPABASE_KEY}" --namespace=${NAMESPACE}
 kubectl create secret generic rabbitmq --from-literal=username="${RABBITMQ_USERNAME}" --from-literal=password="${RABBITMQ_PASSWORD}" --namespace=${NAMESPACE}
